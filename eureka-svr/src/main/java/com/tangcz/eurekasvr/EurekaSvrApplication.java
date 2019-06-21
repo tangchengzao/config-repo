a09cb3055948eb524d505fc7e6c20aecff75171b
月汐 <tangchengzao@hzchengzi.com>
2019-06-21 18:24:56 +0800

    提交

--- eureka-svr/src/main/java/com/tangcz/eurekasvr/EurekaSvrApplication.java
+++ eureka-svr/src/main/java/com/tangcz/eurekasvr/EurekaSvrApplication.java
@@ -2,8 +2,10 @@ package com.tangcz.eurekasvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaSvrApplication {
 
     public static void main(String[] args) {
